f8accfe49e6dbc29662161fdaede6b21f8cc4696
Oghenetega Peter-Saduwa <Oghenetega.peter@wemabank.com>
2020-05-18 00:50:41 +0100

    Updates

---
+++ precache-manifest.33c3c25b9a3ccdba1b68163215ba658c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a60df3fb85d0dfb0b3dd430d64ec3e",
+    "url": "/MonsterReloaded/index.html"
+  },
+  {
+    "revision": "297ceb41c2a961447605",
+    "url": "/MonsterReloaded/static/css/main.90973d06.chunk.css"
+  },
+  {
+    "revision": "93def368ba2567f23b14",
+    "url": "/MonsterReloaded/static/js/2.16eb62f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterReloaded/static/js/2.16eb62f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297ceb41c2a961447605",
+    "url": "/MonsterReloaded/static/js/main.eedfb478.chunk.js"
+  },
+  {
+    "revision": "4aad4cc494d485cefcff",
+    "url": "/MonsterReloaded/static/js/runtime-main.4ac9c5cd.js"
+  }
+]);
